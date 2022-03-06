2bfdc5ab89aac5c2adc3e3db4e31a864feed1bcb
garretthilberling <ghilberling@knights.ucf.edu>
2022-03-05 22:48:07 -0500

    nearly finished with application

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,18 +1,32 @@
 const router = require('express').Router();
-const { notes } = require('../../db/db');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+var { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
-let results = notes;
-res.json(results);
+  for (let i = 0; i < notes.length; i++) {
+    notes[i].id = uuidv4(); 
+  }
+  console.log(uuidv4());
+  res.json(notes);
 });
 
-router.delete('/notes', (req, res) => {
-const id = res.notes.id;
+router.delete('/notes/:id', (req, res) => {
+  notes = filterDelete(notes, req.params.id);
+  res.json(notes);
 });
+
+function filterDelete (notes, id) {
+  for (let i = 0; i < notes.length; i++) {
+    if(notes[i].id == id) {
+      delete notes[i]
+    }
+  }
+  return notes.filter(() => true);
+}
 
 router.post('/notes', (req, res) => {
-    req.body.id = uuidv4(); 
+  notes.push(req.body);
 });
 
 module.exports = router;
